e5cab9eef87dc6b061f83aaaf1b275b19b696760
michaelifebrian <68809822+michaelifebrian@users.noreply.github.com>
2023-11-16 17:17:29 +0700

    library pendukung

---
+++ fps-nanocamera.py
@@ -0,0 +1,62 @@
+from models import TRTModule  # isort:skip
+import argparse
+from pathlib import Path
+
+import cv2
+import torch
+import time
+import imutils
+import nanocamera as nano
+
+from config import CLASSES, COLORS
+from models.torch_utils import det_postprocess
+from models.utils import blob, letterbox, path_to_list
+
+
+device = torch.device("cuda:0")
+Engine = TRTModule("best_416_sim.engine", device)
+H, W = Engine.inp_info[0].shape[-2:]
+
+# set desired output names order
+Engine.set_desired(['num_dets', 'bboxes', 'scores', 'labels'])
+
+camera = nano.Camera(camera_type=1, device_id=0, width=640, height=480, fps=30)
+prev_time = 0
+new_time = 0
+
+while camera.isReady():
+    # Read a frame from the video
+    frame = camera.read()
+    if True:
+        bgr, ratio, dwdh = letterbox(frame, (W, H))
+        rgb = cv2.cvtColor(bgr, cv2.COLOR_BGR2RGB)
+        tensor = blob(rgb, return_seg=False)
+        dwdh = torch.asarray(dwdh * 2, dtype=torch.float32, device=device)
+        tensor = torch.asarray(tensor, device=device)
+        # inference
+        #data = Engine(tensor)
+        #bboxes, scores, labels = det_postprocess(data)
+        #bboxes -= dwdh
+        #bboxes /= ratio
+        #for (bbox, score, label) in zip(bboxes, scores, labels):
+        #    if score > 0.25:
+        #        bbox = bbox.round().int().tolist()
+        #        cls_id = int(label)
+        #        cls = CLASSES[cls_id]
+        #        color = COLORS[cls]
+        #        cv2.rectangle(frame, bbox[:2], bbox[2:], color, 2)
+        #        cv2.putText(frame,
+        #                    f'{cls}:{score:.3f}', (bbox[0], bbox[1] - 2),
+        #                    cv2.FONT_HERSHEY_SIMPLEX,
+        #                    0.75, [225, 255, 255],
+        #                    thickness=2)
+        new_time = time.time()
+        fps = int(1/(new_time-prev_time))
+        prev_time = new_time
+        print("FPS: "+str(fps))
+        cv2.imshow("Output", frame)
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            break
+camera.release()
+del camera
---
+++ fps-opencv.py
@@ -0,0 +1,67 @@
+from models import TRTModule  # isort:skip
+import argparse
+from pathlib import Path
+
+import cv2
+import torch
+import time
+import imutils
+
+from config import CLASSES, COLORS
+from models.torch_utils import det_postprocess
+from models.utils import blob, letterbox, path_to_list
+
+pipeline = " ! ".join(["v4l2src device=/dev/video0",
+                       "video/x-raw, width=640, height=480, framerate=30/1",
+                       "videoconvert",
+                       "video/x-raw, format=(string)BGR",
+                       "appsink"
+                       ])
+
+device = torch.device("cuda:0")
+Engine = TRTModule("best_416_sim.engine", device)
+H, W = Engine.inp_info[0].shape[-2:]
+
+# set desired output names order
+Engine.set_desired(['num_dets', 'bboxes', 'scores', 'labels'])
+
+video_capture = cv2.VideoCapture(pipeline, cv2.CAP_GSTREAMER)
+prev_time = 0
+new_time = 0
+
+while video_capture.isOpened():
+    # Read a frame from the video
+    ret_val, frame = video_capture.read()
+    if ret_val:
+        bgr, ratio, dwdh = letterbox(frame, (W, H))
+        rgb = cv2.cvtColor(bgr, cv2.COLOR_BGR2RGB)
+        tensor = blob(rgb, return_seg=False)
+        dwdh = torch.asarray(dwdh * 2, dtype=torch.float32, device=device)
+        tensor = torch.asarray(tensor, device=device)
+        # inference
+        #data = Engine(tensor)
+        #bboxes, scores, labels = det_postprocess(data)
+        #bboxes -= dwdh
+        #bboxes /= ratio
+        #for (bbox, score, label) in zip(bboxes, scores, labels):
+        #    if score > 0.25:
+        #        bbox = bbox.round().int().tolist()
+        #        cls_id = int(label)
+        #        cls = CLASSES[cls_id]
+        #        color = COLORS[cls]
+        #        cv2.rectangle(frame, bbox[:2], bbox[2:], color, 2)
+        #        cv2.putText(frame,
+        #                    f'{cls}:{score:.3f}', (bbox[0], bbox[1] - 2),
+        #                    cv2.FONT_HERSHEY_SIMPLEX,
+        #                    0.75, [225, 255, 255],
+        #                    thickness=2)
+        new_time = time.time()
+        fps = int(1/(new_time-prev_time))
+        prev_time = new_time
+        print("FPS: "+str(fps))
+        cv2.imshow("Output", frame)
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            break
+video_capture.release()
+cv2.destroyAllWindows()
